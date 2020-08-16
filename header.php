574aa2fcf540a140be51859d556d9a50df68f610
Mateusz S <mateuszuk1992@gmail.com>
2020-08-16 12:39:03 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+     <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  
+    <header>
+        <div class="header-cont">
+        <div class="space">
+        <div class="logo">
+            
+        <a href="#"><img src="images/logo.png" alt="logo"></a>
+        </div>
+        
+        <nav>
+            <ul class="msnav-list">
+                                       
+                    <li><a href="index.php" class="msnav-link">Home</a>
+                    <li><a href="index.php" class="msnav-link">Home</a>
+                    <li><a href="index.php" class="msnav-link">Home</a>
+                        
+            </ul>
+        </nav>
+            </div>
+            </div>
+            <img src="images/header.png" alt="logo" class="headerbgms">
+    </header>
+
+</body>
+
+</html> 
